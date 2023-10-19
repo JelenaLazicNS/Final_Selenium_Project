1fe5b7d746130bbad1485f584235915b978a56fb
Jelena Lazic <jelena.lazic.novi.sad@gmail.com>
2023-10-19 19:02:03 +0200

    Add Retry Analyzer

--- src/main/java/Retry/RetryAnalyzer.java
+++ src/main/java/Retry/RetryAnalyzer.java
@@ -1,4 +1,19 @@
 package Retry;
 
-public class RetryAnalyzer {
+import org.testng.ITestResult;
+import org.testng.IRetryAnalyzer;
+
+
+public class RetryAnalyzer implements IRetryAnalyzer {
+    private int maxRetry = 3;
+    private int count = 0;
+
+    public boolean retry (ITestResult iTestResult) {
+        if (count<maxRetry){
+            count++;
+            return true;
+        }
+        return false;
+    }
+
 }
