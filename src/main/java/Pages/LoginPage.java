e763b71b516198589fdc2fff7c1c5babcb9924ed
Jelena Lazic <jelena.lazic.novi.sad@gmail.com>
2023-10-19 19:52:59 +0200

    Add Login Page

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -1,22 +1,21 @@
 package Pages;
 
-import org.apache.commons.logging.Log;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage extends BasicPage{
-    public LoginPage(WebDriver driver, WebDriverWait wait){
+    public LoginPage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
     }
-    public WebElement getEmailField () {
-        return driver.findElement(By.id("email"));
+    public static WebElement getEmailInput() {
+        return driver.findElement(By.cssSelector("input[type='email']"));
     }
-    public WebElement getPasswordField () {
-        return driver.findElement(By.id("password"));
+    public static WebElement getPasswordInput() {
+        return driver.findElement(By.cssSelector("input[type='password']"));
     }
-    public WebElement getLoginButton () {
-        return driver.findElement(By.className("v-btn__content"));
-    }
+
+
 }
