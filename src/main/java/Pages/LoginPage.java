1d47954d74320bd9be2ba74a6c8d8719013285d2
Jelena Lazic <jelena.lazic.novi.sad@gmail.com>
2023-10-25 19:43:57 +0200

    Displays errors when user does not exist

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -6,16 +6,34 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class LoginPage extends BasicPage{
+public class LoginPage extends BasicPage {
     public LoginPage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
     }
+
     public static WebElement getEmailInput() {
         return driver.findElement(By.cssSelector("input[type='email']"));
     }
+
     public static WebElement getPasswordInput() {
         return driver.findElement(By.cssSelector("input[type='password']"));
     }
 
+    public static WebElement getLoginButton(){
+        return driver.findElement(By.cssSelector("div.flex.text-xs-center.mt-5.mb-3 > button"));
+    }
+    public static void clickOnLoginButton() {
+        getLoginButton().click();
+    }
 
+    public static void waitForErrorPopupToBeVisible() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.v-snack__content")));
+    }
+
+    public static String getErrorLoginPopupMessage() {
+        WebElement errorElement = driver.findElement(By.cssSelector("div > div.v-snack__content > ul > li"));
+        return errorElement.getText();
+
+    }
 }
+
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -11,17 +11,34 @@ public class LoginTest extends BasicTest {
         navPage.clickOnLanguageButton();
         navPage.getEnglishLanguage();
         navPage.clickOnEnglishLanguage();
-        navPage.getLoginButton();
-        navPage.clickOnLoginButton();
+        navPage.getLoginNavButton();
+        navPage.clickOnLoginNavButton();
         Assert.assertEquals(driver.getCurrentUrl(),
                 baseUrl + "/login");
     }
     @Test (priority = 2, retryAnalyzer = RetryAnalyzer.class)
     public void checksInputTypes(){
-        navPage.clickOnLoginButton();
+        navPage.clickOnLoginNavButton();
         String attributeEmail = LoginPage.getEmailInput().getAttribute("type");
         String attributePassword =LoginPage.getPasswordInput().getAttribute("type");
         Assert.assertEquals(attributeEmail, "email", "The email field should have the value \"email\" in the \"type\" attribute." );
         Assert.assertEquals(attributePassword, "password", "The password field should have the value \"password\" in the \"type\" attribute.");
     }
+    @Test (priority = 3, retryAnalyzer = RetryAnalyzer.class)
+    public void displaysErrorsWhenUserDoesNotExist(){
+        String email = "non-existing-user@gmal.com";
+        String password = "password123";
+        navPage.clickOnLoginNavButton();
+
+        LoginPage.getEmailInput().sendKeys(email);
+        LoginPage.getPasswordInput().sendKeys(password);
+        LoginPage.clickOnLoginButton();
+        LoginPage.waitForErrorPopupToBeVisible();
+
+        String errorMessage = LoginPage.getErrorLoginPopupMessage();
+        Assert.assertEquals(errorMessage, "User does not exists", "Message from popup should be like expected message");
+
+        Assert.assertEquals(driver.getCurrentUrl(), baseUrl + "/login", "Urls should be similar.");
+    }
+
 }
