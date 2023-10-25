f01be62496c6ed97bf2aef804f06adb16581bc17
Jelena Lazic <jelena.lazic.novi.sad@gmail.com>
2023-10-25 22:27:01 +0200

    Forbids to visit home page if url is not authenticated test

---
+++ src/test/java/tests/AuthRoutesTest.java
@@ -0,0 +1,13 @@
+package tests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import Retry.RetryAnalyzer;
+
+
+public class AuthRoutesTest extends BasicTest{
+    @Test (priority = 1, retryAnalyzer = RetryAnalyzer.class)
+    public void visitsHomePage() throws InterruptedException{
+        driver.get(baseUrl + "/home");
+        Assert.assertEquals(driver.getCurrentUrl(), baseUrl + "/login", "Current url should be " + baseUrl + "/login");
+    }
+}
